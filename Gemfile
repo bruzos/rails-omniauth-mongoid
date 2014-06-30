f50d847c56ce868708bda62da719ce52233af2ca
Daniel Kehoe <daniel@danielkehoe.com>
2014-06-30 13:55:03 -0700

    changes required to use Mongoid

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'bootstrap-sass'
 gem 'high_voltage'
 gem 'omniauth'
 gem 'omniauth-twitter'
+gem 'mongoid'
+
 group :development do
   gem 'better_errors'
   gem 'binding_of_caller', :platforms=>[:mri_21]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
-class User < ActiveRecord::Base
+class User
+  include Mongoid::Document
+  include Mongoid::Timestamps::Created
+  field :provider, type: String
+  field :uid, type: String
+  field :name, type: String
+  # run 'rake db:mongoid:create_indexes' to create indexes
+  index({ email: 1 }, { unique: true, background: true })
 
   def self.create_with_omniauth(auth)
     create! do |user|
